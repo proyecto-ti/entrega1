d660f26a68e92d46db82a395b9924869268a3d02
Nicolas <nnaguirre@uc.cl>
2019-04-29 18:35:34 -0400

    Subiendo mejora

--- proj/api/funciones_bodega.py
+++ proj/api/funciones_bodega.py
@@ -3,6 +3,7 @@ from hashlib import sha1
 import hmac
 import base64
 import json
+from .poblamiento import excel_final
 
 api_key = 'A#soL%kRvHX2qHm'
 api_url_base = 'https://integracion-2019-dev.herokuapp.com/bodega/'
@@ -79,7 +80,6 @@ def update_dictionary_stocks(dictionary, stock_type):
             dictionary[sku["_id"]] += sku["total"]
     return dictionary
 
-
 def obtener_productos_almacen(almacenId, sku):
     message = 'GET' + almacenId + sku
     headers = {'Content-Type': 'application/json',
@@ -87,8 +87,6 @@ def obtener_productos_almacen(almacenId, sku):
     url = '{}stock?almacenId={}&sku={}'.format(api_url_base, almacenId, sku)
     result = requests.get(url, headers=headers).json()
     return result
-
-
 
 def obtener_id_producto(sku, cantidad, almacenId):
     #lista de almacenes con el producto
@@ -107,8 +105,6 @@ def obtener_id_producto(sku, cantidad, almacenId):
     # en caso de que la cantidad sea mayor que lo que se tiene, igual se entrega la lista con todos los existentes
     return lista_id_productos
 
-
-
 def mover_entre_almacenes(sku, cantidad, almacenId_origen, almacenId_destino):
     lista_id = obtener_id_producto(sku, cantidad, almacenId_origen)
     for productoId in lista_id:
@@ -120,15 +116,126 @@ def mover_entre_almacenes(sku, cantidad, almacenId_origen, almacenId_destino):
 
         requests.post(url, headers=headers_, data=json.dumps(body))
 
-def mover_entre_almacenes(sku, cantidad, almacenId_origen, almacenId_destino):
-    lista_id = obtener_id_producto(sku, cantidad, almacenId_origen)
-    for productoId in lista_id:
-        message = 'POST' + productoId + almacenId_destino
-        url = '{}moveStock'.format(api_url_base)
-        headers_ = {'Content-Type': 'application/json',
-                    'Authorization': 'INTEGRACION grupo2:{}'.format(sign_request(message))}
-        body = {"productoId": productoId, "almacenId": almacenId_destino}
+def pedir_productos_sku(sku, cantidad):
+    productos = excel_final() #lista de los productos
+    productores = productos[sku].productores
+    total_pedido = 0
+    for grupo in productores:
+        try:
+            result = get_inventories_grupox(grupo)
+            #print("GRUPO", str(grupo), "esta con STA_CODE:", result.status_code)
+            if grupo != 2 and result.status_code == 200:
+                for resultados in result.json():
+                    if resultados['sku'] == sku:
+                        result_2 = post_orders_grupox(grupo, cantidad, sku)
+                        print(result_2)
+            else:
+                pass
+        except:
+            pass
 
-        requests.post(url, headers=headers_, data=json.dumps(body))
+def cantidad_producto(sku):
+    cantidad = 0
+    for almacen in almacen_id_dict:
+        lista_productos = ObtenerSkuconStock(almacen_id_dict[almacen])
+        for producto in lista_productos:
+            if producto['_id'] == sku:
+                cantidad += producto['total']
+                break
+    return cantidad
+#pedir_productos_sku('1001', 1)
+
+def get_inventories_grupox(grupo):
+    url = 'http://tuerca' + str(grupo) + '.ing.puc.cl/inventories/'
+    headers_ = {'Content-Type': 'application/json'}
+    result = requests.get(url, headers=headers_)
+    return result
+
+def post_orders_grupox(grupo, cantidad, sku):
+    url = 'http://tuerca' + str(grupo) + '.ing.puc.cl/orders/'
+    headers_ = {'Content-Type': 'application/json'}
+    body = {'sku': sku, 'cantidad': cantidad, 'almacenId': almacen_id_dict['recepcion']}
+    result = requests.post(url, headers=headers_, data=json.dumps(body))
+    return result
+try:
+    #print(get_inventories_grupox(2).json())
+    print(pedir_productos_sku('1001', 1))
+    #print(get_inventories_grupox(2).json())
+except:
+    print("dias")
 
 
+def liberar_recepcion():
+    #funcion que deja recepcion vacia, se mandan productos a almcanen1 o almacen2
+    datos_bodegas = revisarBodega()
+    #espacio en recepcion
+    espacio_usado = datos_bodegas[0]['usedSpace']
+    if espacio_usado == 0:
+        #recepcion ya esta vacia
+        return
+    else:
+        lista_recepcion = ObtenerSkuconStock("5cbd3ce444f67600049431b9")
+        for producto in lista_recepcion:
+            #se actualizan datos de bodega
+            datos_bodegas = revisarBodega()
+            #revisa si cabe en almacen1
+            if producto['total'] <= datos_bodegas[2]['totalSpace'] - datos_bodegas[2]['usedSpace']:
+                #traspasa todos esos productos a almacen1
+                mover_entre_almacenes(producto['_id'], producto['total'], "5cbd3ce444f67600049431b9", "5cbd3ce444f67600049431bb")
+            #revisa si cabe en almacen2
+            elif producto['total'] <= datos_bodegas[3]['totalSpace'] - datos_bodegas[3]['usedSpace']:
+                #traspasa todos esos productos a almacen2
+                mover_entre_almacenes(producto['_id'], producto['total'], "5cbd3ce444f67600049431b9", "5cbd3ce444f67600049431bc")
+    return
+
+
+def despachar_producto(sku, cantidad):
+    #cantidad que se ha envidado a despacho
+    datos_bodegas = revisarBodega()
+    capacidad_despacho = datos_bodegas[1]['totalSpace'] - datos_bodegas[1]['usedSpace']
+    if capacidad_despacho == 0:
+        return
+    elif capacidad_despacho >= cantidad:
+        cantidad_despachar = cantidad
+    else:
+        cantidad_despachar = capacidad_despacho
+    #revisa si producto esta en pulmon
+    lista_pulmon = ObtenerSkuconStock("5cbd3ce444f67600049431bd")
+    for producto in lista_pulmon:
+        if producto['_id'] == sku:
+            if producto['total'] >= cantidad_despachar:
+                # se tiene la cantidad que se necesita
+                mover_entre_almacenes(sku, cantidad_despachar, "5cbd3ce444f67600049431bd", "5cbd3ce444f67600049431ba")
+                return
+            elif producto['total'] < cantidad_despachar:
+                # no se tiene la cantidad que se necesita, se manda lo que se tiene
+                mover_entre_almacenes(sku, producto['total'], "5cbd3ce444f67600049431bd", "5cbd3ce444f67600049431ba")
+                cantidad_despachar -= producto['total']
+            break
+    #revisa si producto esta en almacen1
+    lista_almacen1 = ObtenerSkuconStock("5cbd3ce444f67600049431bb")
+    for producto in lista_almacen1:
+        if producto['_id'] == sku:
+            if producto['total'] >= cantidad_despachar:
+                #se tiene la cantidad que se necesita
+                mover_entre_almacenes(sku, cantidad_despachar, "5cbd3ce444f67600049431bb", "5cbd3ce444f67600049431ba")
+                return
+            elif producto['total'] < cantidad_despachar:
+                #no se tiene la cantidad que se necesita, se manda lo que se tiene
+                mover_entre_almacenes(sku, producto['total'], "5cbd3ce444f67600049431bb", "5cbd3ce444f67600049431ba")
+                cantidad_despachar -= producto['total']
+            break
+    #revisa si producto esta en almacen2
+    lista_almacen2 = ObtenerSkuconStock("5cbd3ce444f67600049431bc")
+    for producto in lista_almacen2:
+        if producto['_id'] == sku:
+            if producto['total'] >= cantidad_despachar:
+                # se tiene la cantidad que se necesita
+                mover_entre_almacenes(sku, cantidad_despachar, "5cbd3ce444f67600049431bc", "5cbd3ce444f67600049431ba")
+                return
+            elif producto['total'] < cantidad_despachar:
+                # no se tiene la cantidad que se necesita, se manda lo que se tiene
+                mover_entre_almacenes(sku, producto['total'], "5cbd3ce444f67600049431bc", "5cbd3ce444f67600049431ba")
+                cantidad_despachar -= producto['total']
+            break
+    return
--- proj/api/tasks.py
+++ proj/api/tasks.py
@@ -1,6 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 from celery import task
+import requests
+
 
 @task()
-def probando():
-    print("fdafasfa")
+def get_inventories_grupoxx():
+    url = 'http://tuerca' + str(2) + '.ing.puc.cl/inventories/'
+    headers_ = {'Content-Type': 'application/json'}
+    result = requests.get(url, headers=headers_)
+    print(result.json())
