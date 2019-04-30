c8d3b508489a71cf8306b6be9b6da0f8dce939c4
matnagra <monavarrete@uc.cl>
2019-04-30 13:48:55 -0400

    se arregla error de status en funcion ordersView

--- proj/api/views.py
+++ proj/api/views.py
@@ -31,7 +31,7 @@ class OrdersView(APIView):
         cantidad = request.data.get("cantidad")
         almacenId = request.data.get("almacenId")
         if not sku or not cantidad or not almacenId:
-            return Response(data="No se creó el pedido por un error del cliente en la solicitud", status.HTTP_400_BAD_REQUEST)
+            return Response(data="No se creó el pedido por un error del cliente en la solicitud", status=status.HTTP_400_BAD_REQUEST)
 
         inventario = get_inventories_grupox(2).json()
         if sku in inventario.keys():
@@ -40,7 +40,7 @@ class OrdersView(APIView):
             if envio.status_code == 200:
                 dictionary = {"sku": sku, "cantidad": cantidad, "almacenId": almacenId, "grupoProveedor": "2", "aceptado": True, "despachado": True}
                 return Response(json.dumps(dictionary))
-            else
-                return Response(data="error de nuestro grupo. Oops!", status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(data="error de nuestro grupo. Oops!", status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(data="Producto no se encuentra", status.HTTP_404_NOT_FOUND)
+            return Response(data="Producto no se encuentra", status=status.HTTP_404_NOT_FOUND)
